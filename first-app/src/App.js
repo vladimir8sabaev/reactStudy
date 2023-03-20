a970c732785f065ac26593c03ff6adb4f0decc14
Vladimir Sabaev <vladimir8sabaev@mail.ru>
2023-03-20 09:41:02 +0300

    feat: learn basics about react components

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+	return <h2>Hello world</h2>;
+};
+// const Field = () => {
+// 	const holder = "Enter here";
+// 	const styledField = {
+// 		width: "300px",
+// 	};
+// 	return <input placeholder={holder} type="text" style={styledField} />;
+// };
+
+class Field extends Component {
+	render() {
+		const holder = "Enter here";
+		const styledField = {
+			width: "300px",
+		};
+		return <input placeholder={holder} type="text" style={styledField} />;
+	}
 }
 
+function Btn() {
+	const text = "Log in";
+	const logged = false;
+	return <button>{logged ? "Enter" : text}</button>;
+}
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Field />
+			<Btn />
+		</div>
+	);
+}
 export default App;
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App";
-
-const text = "Hello world";
-function sum() {
-	return 30;
-}
-const elem = (
-	<div>
-		<h2 tabIndex={0}>Text: {text + " " + sum()}</h2>
-		<input type="text" />
-		<button>Click</button>
-	</div>
-);
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elem);
+root.render(<App />);
